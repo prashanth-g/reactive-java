c3bcfd752e1709361908178b29f3204ea571f9c3
prashanth-g <prashanthcsgce@gmail.com>
2019-09-17 20:51:05 +0530

    Updated with schedulers

--- src/main/java/com/prashanth/os/reactive/ReactAction.java
+++ src/main/java/com/prashanth/os/reactive/ReactAction.java
@@ -2,35 +2,46 @@ package com.prashanth.os.reactive;
 
 import java.util.*;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
+import rx.schedulers.Schedulers;
 
 public class ReactAction {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
 
     List<String> stocks = Arrays.asList("GOOG", "AAPL", "AMZN", "WMT");
     final Observable<StockInfo> feed = Stockserver.getFeed(stocks);
     // feed.subscribe(System.out::println);
 
-    feed.subscribe(new Subscriber<StockInfo>() {
-      @Override
-      public void onCompleted() {
-        System.out.println("Completed!");
-      }
+//    feed.subscribe(new Subscriber<StockInfo>() {
+//      @Override
+//      public void onCompleted() {
+//        System.out.println("Completed!");
+//      }
+//
+//      @Override
+//      public void onError(Throwable e) {
+//        System.out.println("An error occurred!");
+//      }
+//
+//      @Override
+//      public void onNext(StockInfo stockInfo) {
+//        System.out.println(stockInfo);
+//        if(stockInfo.price > 5.0d) {
+//          System.out.println("Lets trade...");
+//          unsubscribe();
+//        }
+//      }
+//    });
 
-      @Override
-      public void onError(Throwable e) {
-        System.out.println("An error occurred!");
-      }
+    feed.subscribeOn(Schedulers.io())
+        .subscribe(ReactAction::printStockInfo);
+    Thread.sleep(10000);
+  }
 
-      @Override
-      public void onNext(StockInfo stockInfo) {
-        System.out.println(stockInfo);
-        if(stockInfo.price > 5.0d) {
-          System.out.println("Lets trade...");
-          unsubscribe();
-        }
-      }
-    });
+  private static void printStockInfo(StockInfo stockInfo) {
+    System.out.println("Thread : " + Thread.currentThread());
+    System.out.println(stockInfo);
   }
 }
